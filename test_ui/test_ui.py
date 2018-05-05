c083e895073c62334026a5030b8af1fb7db626e7
ktrojanek89 <ktrojanek89@users.noreply.github.com>
2018-05-05 14:05:30 +0200

    zmiana7 05.05

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -1,17 +1,20 @@
+# PO PROSTU SKRYP
 from selenium import webdriver
-import unittest
-import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
-# mkdir -p test_ui
-# atom test_ui/test_ui.py
-#
-# https://github.com/wojciech11/se_intro_selenium
-#
+driver = webdriver.Chrome()
+driver.get("https://encrypted.google.com")
+assert "Google" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("chomik")
+elem.send_keys(Keys.ENTER)
 
-@pytest.mark.webtestst
-class TestFormater(unittest.TestCase):
-    def test_plain_lowercase(self):
-        driver = webdriver.Chrome()
-        driver.get("http://127.0.0.1:5000/ui")
-        time.sleep(1000)
+wait = WebDriverWait(driver, 10)
+
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "res")))
+
+driver.quit()

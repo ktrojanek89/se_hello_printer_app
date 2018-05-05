7243336ea8925ce55071d43f51a07a740b72492a
ktrojanek89 <ktrojanek89@users.noreply.github.com>
2018-05-05 14:51:33 +0200

    zmiana koniec

--- test_ui/test_ui.py
+++ test_ui/test_ui.py
@@ -1,20 +1,38 @@
 # PO PROSTU SKRYP
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.by import By
+
+
+#driver = webdriver.Chrome()
+#driver.get("https://encrypted.google.com")
+#assert "Google" in driver.title
+#elem = driver.find_element_by_name("q")
+#elem.send_keys("chomik")
+#elem.send_keys(Keys.ENTER)
+
+#wait = WebDriverWait(driver, 10)
+
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "res")))
+
+#driver.quit()
+
+#jako unit test 
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import unittest
+import time
+import pytest
 
 
-driver = webdriver.Chrome()
-driver.get("https://encrypted.google.com")
-assert "Google" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("chomik")
-elem.send_keys(Keys.ENTER)
-
-wait = WebDriverWait(driver, 10)
-
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "res")))
-
-driver.quit()
+@pytest.mark.uitest
+class TestFormater(unittest.TestCase):
+    def test_plain_lowercase(self):
+        driver = webdriver.Chrome()
+        driver.get("http://127.0.0.1:5000/ui")
+        link = driver.find_element_by_xpath("/html/body/a")
+        link.click()
+        time.sleep(100)
+        driver.quit()

ab1f0de8956ca4fafbf53c2da119989a18d2f028
ktrojanek89 <ktrojanek89@users.noreply.github.com>
2018-05-15 20:57:00 +0200

    21/15/05/18

--- test-api/api_test.py
+++ test-api/api_test.py
@@ -20,7 +20,7 @@ def main():
 
 def checkOutput(rj):
     actual = jmespath.search('imie', rj)
-    expected = "Karolina"
+    expected = "Simon's saying"
     if actual != expected:
         print("FAILED: We expected: " + expected + " but was " + actual)
         exit(1)
